b0c2aac91225571244f99f4ac7706c181e893d06
cup-noodlehS <93570629+cup-noodlehS@users.noreply.github.com>
2023-02-09 16:21:24 +0800

    ready to  try deploy

--- public/javaScripts/clusterMap.js
+++ public/javaScripts/clusterMap.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
   container: "cluster-map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/light-v11",
-  center: [-103.5917, 40.6699],
+  center: [120, 20],
   zoom: 3,
 });
 
